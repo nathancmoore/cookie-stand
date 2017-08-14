f527a2d6f1d76cac487ca22becfa1bdc7a108afa
Nathan Moore <ncmooore1986@gmail.com>
2017-08-14 15:03:28 -0700

    Added the rest of the store data to my objects and called the function 5 times.

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 'use strict';
 
 var salesFirstAndPike = [];
+var salesSeaTacAirport = [];
+var salesSeattleCenter = [];
+var salesCapitolHill = [];
+var salesAlki = [];
 
 var firstAndPike = {
+  name: 'First and Pike',
   min: 23,
   max: 65,
   avgCust: 6.3,
 };
+var seaTacAirport = {
+  name: 'SeaTacAirport',
+  min: 3,
+  max: 24,
+  avgCust: 1.2,
+};
+var seattleCenter = {
+  name: 'Seattle Center',
+  min: 11,
+  max: 38,
+  avgCust: 3.7,
+};
+var capitolHill = {
+  name: 'Capitol Hill',
+  min: 20,
+  max: 38,
+  avgCust: 2.3,
+};
+var alki = {
+  name: 'Alki',
+  min: 2,
+  max: 16,
+  avgCust: 4.6,
+};
 
 function generateRandom(obj, array) {
+  array.push('Generated sales for ' + obj.name + ':');
   for (var i = 6; i < 12; i++) {
     var randomCust = Math.floor(Math.random() * (obj.max - obj.min) + obj.min);
     array.push(i + 'am: ' + (randomCust * obj.avgCust) + ' cookies');
@@ -24,11 +54,7 @@ function generateRandom(obj, array) {
 }
 
 generateRandom(firstAndPike, salesFirstAndPike);
-// var seaTacAirport
-//
-// var seattleCenter
-//
-// var capitolHill
-//
-// var alki
-//
+generateRandom(seaTacAirport, salesSeaTacAirport);
+generateRandom(seattleCenter, salesSeattleCenter);
+generateRandom(capitolHill, salesCapitolHill);
+generateRandom(alki, salesAlki);
